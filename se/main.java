ab8983a6564334db87bb02f077a139d8b697e28b
WonJuna <43838026+WonJuna@users.noreply.github.com>
2019-12-03 16:07:01 +0900

    Update main.java

--- se/main.java
+++ se/main.java
@@ -3,6 +3,7 @@ package se;
 // 1: remote by WonJuna
 // 2: local by WonJuna
 // 2: remote by WonJuna
+// 1: localBranch01 by WonJuna
 public class main {
 
 	public static void main(String[] args) {
